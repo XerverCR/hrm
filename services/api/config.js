728d152b41c4cc22e3811554b3e5920d0dc36547
jpmendezf <49535323+jpmendezf@users.noreply.github.com>
2021-08-24 14:03:06 -0600

    aaaaaaaaa

---
+++ services/api/config.js
@@ -0,0 +1,15 @@
+
+export const googlemapApiForAutoCheckPoint= "AIzaSyBRAPDJ_UsEhDcdNSvDNtdLdZQmMSTIboc";
+
+
+ export const urlDev = "https://oficinasonline.com/api/ResourceTracker/"; //replace your api
+ export const urlResource = "https://oficinasonline.com/UploadFiles/"; //replace your api
+
+export var initialUrl = urlDev;
+export default {
+  clientId: "8puWuJWZYls1Ylawxm6CMiYREhsGGSyw",
+  url: initialUrl,
+  bannerUnitId: "",
+  interstitialUnitId: "",
+  rewardedUnitId: ""
+};
